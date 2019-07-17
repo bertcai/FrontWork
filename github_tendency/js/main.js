23a09ab80bf9cf73e9bb3c6459dc1fea3acb4572
bert_cai <bert_cai@163.com>
2019-07-17 16:19:35 +0800

    add loading

--- github_tendency/js/main.js
+++ github_tendency/js/main.js
@@ -6,10 +6,13 @@ $('footer div').on('click', function () {
 })
 
 let index = 0
+let loading = false
 
 getData()
 
 function getData() {
+    if (loading) return
+    loading = true
     $.ajax({
         url: 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc',
         type: 'get',
@@ -22,6 +25,8 @@ function getData() {
         index++
     }).fail(function () {
         console.log('fail...')
+    }).always(function () {
+        loading = false
     })
 }
 
@@ -41,17 +46,21 @@ function create(res) {
                                 </div>
                             </a>
                         </div>`)
-        $item.find('.order span').text(index*30+i+1)
+        $item.find('.order span').text(index * 30 + i + 1)
         $item.find('.detail h2').text(repo.name)
         $item.find('.detail .description').text(repo.description)
         $item.find('.detail .extra .star-count').text(repo.stargazers_count)
-        $item.find('a').attr('href',repo.html_url)
+        $item.find('a').attr('href', repo.html_url)
         $('.top-repos .container').append($item)
     })
 }
 
-$('main').on('scroll',function(){
-    if($(this).height()+$(this).scrollTop()+30>$('.container').height()){
-        getData()
-    }
+let clock
+$('main').on('scroll', function () {
+    if (clock) clearTimeout(clock)
+    clock = setTimeout(function () {
+        if ($('section').height() + $('section').scrollTop() + 30 > $('.container').height()) {
+            getData()
+        }
+    }, 300)
 })
