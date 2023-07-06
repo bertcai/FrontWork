9221d86bb5ef82bdcebee4b29601e681dd23084e
bert_cai <bert_cai@outlook.com>
2023-07-06 17:00:11 +0800

    node-library:完成挑战1

--- node-library/routes/users.js
+++ node-library/routes/users.js
@@ -5,5 +5,8 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/cool', function(req, res, next) {
+  res.send('You\'re so cool');
+});
 
 module.exports = router;
