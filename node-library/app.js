cd26e809c05519d4d0d786c25a99bb95ec440595
bert_cai <bert_cai@outlook.com>
2023-07-07 15:23:26 +0800

    node-library: 完成catalog路由组织结构

--- node-library/app.js
+++ node-library/app.js
@@ -6,6 +6,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');  // 导入 catalog 路由
 
 let app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog',catalogRouter);  // 将 catalog 路由添加进中间件链
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
