ef5b73ce1256dddd3f5bb30a923a0e50ccc93e22
bert_cai <bert_cai@outlook.com>
2023-07-10 17:23:58 +0800

    node-library: 完成-更新图书副本表单

--- node-library/controllers/bookinstanceController.js
+++ node-library/controllers/bookinstanceController.js
@@ -2,6 +2,7 @@ const BookInstance = require('../models/bookinstance');
 
 const Book = require('../models/book');
 const { body, validationResult } = require('express-validator');
+const async = require('async');
 
 // 显示完整的藏书副本列表
 exports.bookinstance_list = (req, res, next) => {
@@ -91,7 +92,69 @@ exports.bookinstance_delete_post = (req, res, next) => {
 };
 
 // 由 GET 显示更新藏书副本的表单
-exports.bookinstance_update_get = (req, res, next) => { res.send('未实现：藏书副本更新表单的 GET'); };
+exports.bookinstance_update_get = (req, res, next) => {
+    async.parallel({
+        bookinstance: function (callback) {
+            BookInstance.findById(req.params.id)
+                .populate('book')
+                .then((bookinstance) => {
+                    callback(null, bookinstance);
+                });
+        },
+        books: function (callback) {
+            Book.find({}, 'title')
+                .then((books) => {
+                    callback(null, books);
+                });
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+        if (results.bookinstance == null) {
+            const err = new Error('未找到藏书副本');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('bookinstance_form', { title: '更新藏书副本', book_list: results.books, selected_book: results.bookinstance.book._id, bookinstance: {
+            _id: results.bookinstance._id,
+            book: results.bookinstance.book._id,
+            imprint: results.bookinstance.imprint,
+            status: results.bookinstance.status,
+            due_back: results.bookinstance.due_back.toISOString().slice(0, 10)
+        } });
+    });
+};
 
 // 由 POST 处理藏书副本更新操作
-exports.bookinstance_update_post = (req, res, next) => { res.send('未实现：更新藏书副本的 POST'); };
+exports.bookinstance_update_post = [
+    // Validate and sanitize fields.
+    body('book', '藏书必须指定').trim().isLength({ min: 1 }).escape(),
+    body('imprint', '印刷信息必须指定').trim().isLength({ min: 1 }).escape(),
+    body('status').escape(),
+    body('due_back', '无效的日期').optional({ checkFalsy: true }).isISO8601().toDate(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        const errors = validationResult(req);
+        let bookinstance = new BookInstance(
+            {
+                book: req.body.book,
+                imprint: req.body.imprint,
+                status: req.body.status,
+                due_back: req.body.due_back,
+                _id: req.params.id
+            }
+        );
+        if (!errors.isEmpty()) {
+            Book.find({}, 'title')
+                .then((books) => {
+                    res.render('bookinstance_form', { title: '更新藏书副本', book_list: books, selected_book: bookinstance.book._id, errors: errors.array(), bookinstance: bookinstance });
+                });
+            return;
+        } else {
+            BookInstance.findByIdAndUpdate(req.params.id, bookinstance)
+                .then(() => {
+                    res.redirect(bookinstance.url);
+                });
+        }
+    }
+]
