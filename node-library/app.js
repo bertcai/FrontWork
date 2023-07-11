d84af5eb90874acceeac9c642934f157af380641
bert_cai <bert_cai@outlook.com>
2023-07-11 10:47:49 +0800

    node-library: 部署-使用helmet解决csp问题，参见mdn-csp

--- node-library/app.js
+++ node-library/app.js
@@ -14,7 +14,7 @@ let app = express();
 // 设置 Mongoose 连接
 const mongoose = require('mongoose');
 const mongoDB = process.env.MONGODB_URI
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
@@ -23,7 +23,17 @@ db.on('error', console.error.bind(console, 'MongoDB 连接错误：'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(helmet());  // 使用 helmet 中间件
+// 使用 helmet 设置 HTTP 头
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "https://cdn.bootcdn.net"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.bootcdn.net"],
+    imgSrc: ["'self'", "https://cdn.bootcdn.net"],
+    connectSrc: ["'self'", "https://cdn.bootcdn.net"],
+    fontSrc: ["'self'", "https://cdn.bootcdn.net"],
+  },
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,15 +43,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog',catalogRouter);  // 将 catalog 路由添加进中间件链
+app.use('/catalog', catalogRouter);  // 将 catalog 路由添加进中间件链
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
