6e52d4b0f3ff5d83d565e80509d7d91faa6f1630
bert_cai <bert_cai@outlook.com>
2023-08-08 10:45:22 +0800

    nest: 全局模块使用示例

--- nest/nest-test/src/app.module.ts
+++ nest/nest-test/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [PersonModule, AaaModule, BbbModule],
   controllers: [AppController],
   providers: [AppService],
 })
